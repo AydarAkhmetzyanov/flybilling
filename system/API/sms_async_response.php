f2e372eb1d01a131bd9cabffcf75f70344d6571a
aydar@creativestripe.ru <CXquWUMa1>
2013-11-01 04:53:13 +0400

    protocols published

--- system/API/sms_async_response.php
+++ system/API/sms_async_response.php
@@ -1,6 +1,6 @@
 <?php
 
-//localhost:64449/system/WORKERS/sms_async_send.php
+//localhost:64449/system/API/sms_async_response.php
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)) . DS . '..' );
