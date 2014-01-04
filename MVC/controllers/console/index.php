c97602a23234d13e8a8eea09c08462ebb9c37218
TERAB1T <akiselyov@ukr.net>
2014-01-05 00:27:05 +0400

    Хотфикс виджета FB

--- MVC/controllers/console/index.php
+++ MVC/controllers/console/index.php
@@ -6,7 +6,10 @@ class IndexController extends Controller {
 		if(Clients::isAuth()){
 	    $data = array();
         $data['title'] = 'Кабинет пользователя';
-
+		
+		HTML::setUserLanguage('ru');
+        $data['newGuest']=false;
+        $data['locale']='ru_RU';
         
 		renderView('header', $data);
         echo '<body class="page-main">';
