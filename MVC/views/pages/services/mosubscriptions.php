5b906c50982c18e87944fbcdf310c3a1775c2ffa
priceamdgit <palatidis@gmail.com>
2014-03-14 22:09:33 +0400

    mobu2

--- MVC/views/pages/services/mosubscriptions.php
+++ MVC/views/pages/services/mosubscriptions.php
@@ -7,12 +7,11 @@
                   
                 </div>
             </div>
-            <div class="promo-3angl-3" data-stellar-ratio="1.5"  data-stellar-vertical-offset="70"></div>
-            <div class="promo-top-bottom"></div>
+           
         </div>
 
         <div class="container content">
-            <div class="polygon-2" data-stellar-ratio="0.3"  data-stellar-vertical-offset="250"></div>
+            
 
             <div class="content-inner">
                 <h2>Описание услуги</h2>
