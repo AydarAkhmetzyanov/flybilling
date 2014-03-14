43de5ef05cfacadca458188e044637c0b2e0d69e
priceamdgit <palatidis@gmail.com>
2014-03-14 22:39:57 +0400

    kabinet pages

--- MVC/views/pages/administration/index.php
+++ MVC/views/pages/administration/index.php
@@ -10,20 +10,19 @@
 
 <div class="g-hidden"></div>
 
-<div class="top-promo">
+<div class="top-promosub">
             <div class="container">
                 <div class="hero-unit">
+                    <br><br>
                     <h1>Кабинет пользователя</h1>
-                    <div class="promo-3angl-1" data-stellar-ratio="2" data-stellar-vertical-offset="80" data-stellar-horizontal-offset="100"></div>
-                    <div class="promo-3angl-2" data-stellar-ratio="1.5"  data-stellar-vertical-offset="80" data-stellar-horizontal-offset="100"></div>
+                    
                 </div>
             </div>
-            <div class="promo-3angl-3" data-stellar-ratio="1.5"  data-stellar-vertical-offset="70"></div>
-            <div class="promo-top-bottom"></div>
+            
         </div>
 
         <div class="container content">
-            <div class="polygon-2" data-stellar-ratio="0.3"  data-stellar-vertical-offset="250"></div>
+            
 
             <div class="content-inner">
                 <div class="row">
