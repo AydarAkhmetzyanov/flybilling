c7e46041e26169ff81b5f803dd317bba817bd5ea
aydar@creativestripe.ru <CXquWUMa1>
2014-03-17 03:06:39 +0400

    services fixed

--- MVC/models/sessionservices.php
+++ MVC/models/sessionservices.php
@@ -11,7 +11,7 @@ class SessionServices extends Model
         } else {
             $tsql.=", [timestamp] as [localtimestamp]";
         }
-        $tsql.=" FROM ".SCHEMA.".[SessionServices] ORDER BY [ID] DESC WHERE 1=1 ";
+        $tsql.=" FROM ".SCHEMA.".[SessionServices] WHERE 1=1  ";
         if(isset($data['ID'])){
             $tsql.=' AND [ID]=:ID';
             $params['ID']=$data['ID'];
@@ -20,6 +20,7 @@ class SessionServices extends Model
             $tsql.=' AND [client_ID]=:client_ID';
             $params['client_ID']=$data['client_ID'];
         }
+        $tsql.=' ORDER BY [ID] DESC ';
         $statement = Database::getInstance()->prepare($tsql);
         try{
             $statement->execute($params);
