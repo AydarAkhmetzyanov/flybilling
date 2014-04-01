7eaa68aec13ce57e04b516bd73fd49fadb7b5564
aydar@creativestripe.ru <CXquWUMa1>
2014-04-01 21:28:21 +0400

    test

--- system/TESTS/database.php
+++ system/TESTS/database.php
@@ -4,4 +4,4 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)) . DS . '..' );
 require_once (ROOT . DS . 'core' . DS . 'core.php');
 
-echo md5('21234');
+Logger::logError('123');
--- system/core/config.php
+++ system/core/config.php
@@ -8,12 +8,19 @@ if(DEVELOPMENT_ENVIRONMENT==true){
     error_reporting(0); ini_set('display_errors', 'Off'); error_reporting( E_ERROR );
 }
 
-define('API_URL',getenv('API_URL'));
-
-define('SCHEMA',getenv('SCHEMA'));
 
 define('EMPTY_RECIEVER_TEXT','Thanks for SMS');
 
 
 define('pl3_pseudo_async_ru',getenv('pl3_pseudo_async_ru'));
 define('pl3_pseudo_async_ru_mts',getenv('pl3_pseudo_async_ru_mts'));
+
+define('SCHEMA',getenv('SCHEMA'));
+define('DEFAULT_SHARE',getenv('DEFAULT_SHARE'));
+define('API_URL',getenv('API_URL'));
+define('SITE',getenv('SITE'));
+define('BRAND',getenv('BRAND'));
+define('SHORT_BRAND',getenv('SHORT_BRAND'));
+define('EMAIL',getenv('EMAIL'));
+define('ADMIN_LOGIN','Admin');
+define('SECRET','d785c99d298a4e9e6e13fe99e602ef42');
